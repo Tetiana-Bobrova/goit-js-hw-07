9882ebf5128fde38b35610691a0ff56d1d15f1e8
Tetiana-Bobrova <74472422+Tetiana-Bobrova@users.noreply.github.com>
2021-07-23 13:32:53 +0300

    Updata task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,11 @@
+//В HTML есть пустой список ul#ingredients.
+//<ul id="ingredients"></ul>
+//В JS есть массив строк.
+//Напиши скрипт, который для каждого элемента массива ingredients
+//создаст отдельный li, после чего вставит все li за одну операцию в
+//список ul.ingredients.
+//Для создания DOM - узлов используй document.createElement().
+
 const ingredients = [
   'Картошка',
   'Грибы',
@@ -6,3 +14,16 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ingredientListEl = document.querySelector("#ingredients");
+
+const ingredientList = ingredients.map(ingredient => {
+    const ingredientItemEl = document.createElement('li');
+    ingredientItemEl.classList.add('ingredient-item');
+    ingredientItemEl.textContent = ingredient;
+
+    return ingredientItemEl;
+});
+
+ingredientListEl.append(...ingredientList);
+//console.log(ingredientListEl);
